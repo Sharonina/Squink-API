3f2fde56762264e3425bdd051c0ae2e554308400
Sharonina <shar_mdza@hotmail.com>
2023-03-16 16:48:32 -0600

    Creación de modelo de usuario

---
+++ modules/users/models/user.model.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+export interface UserBody {
+  uuid: string;
+}
+export interface IUser extends UserBody {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const userSchema = new mongoose.Schema<IUser>({
+  name: { type: String, default: null, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  uuid: { type: String, required: true },
+});
+
+userSchema.index({ uuid: 1, email: 1 }, { unique: true });
+export const UserModel = mongoose.model("user", userSchema);
