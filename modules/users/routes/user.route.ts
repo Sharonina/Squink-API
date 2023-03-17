658b444358eeec948b98bace6537fd1915fe2681
Sharonina <shar_mdza@hotmail.com>
2023-03-16 20:37:50 -0600

    Correcciones a usuarios route y service

--- modules/users/routes/user.route.ts
+++ modules/users/routes/user.route.ts
@@ -1,8 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
 import { verifyTokenMiddleware } from "../../../middleware/auth";
-import UserService from "../services/user.service";
+import { createUser, getUserById, login } from "../services/user.service";
 
-const userService = new UserService();
 const router = express.Router();
 
 const authenticationMiddlewares = [verifyTokenMiddleware];
@@ -12,8 +11,8 @@ router.get(
   authenticationMiddlewares,
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const token = req.headers["authorization"] || "";
-      const user = await userService.getUserByToken(token);
+      const { user_id } = res.locals.user;
+      const user = await getUserById(user_id);
       res.status(200).send(user);
     } catch (error) {
       next(error);
@@ -24,7 +23,7 @@ router.get(
 router.post("/", async (req: Request, res: Response, next: NextFunction) => {
   try {
     const body = req.body;
-    const user = await userService.createUser(body);
+    const user = await createUser(body);
     res.status(201).json(user);
   } catch (error) {
     next(error);
@@ -36,7 +35,7 @@ router.post(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       const body = req.body;
-      const loginResponse = await userService.login(body);
+      const loginResponse = await login(body);
       res.status(200).send(loginResponse);
     } catch (error) {
       next(error);
--- modules/users/services/user.service.ts
+++ modules/users/services/user.service.ts
@@ -1,99 +1,111 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { uuid } from "uuidv4";
+import mongoose from "mongoose";
 
 import { errorObject } from "../../../utils/errors.utils";
 import { UserBody, UserModel } from "./../models/user.model";
 import config from "../../../config";
 import { Decoded } from "../../../middleware/auth";
 
-export default class UserService {
-  constructor() {} // dejar en caso de querer añadir atributos
-
-  //get user by token
-  async getUserByToken(token: string) {
-    if (!token) {
-      throw errorObject(401, "Access denied. No token provided");
-    }
-    try {
-      const decoded = jwt.verify(token, config.JWT_SECRET) as Decoded;
-      const user = await UserModel.findById(decoded.user_id).exec();
-      if (!user) {
-        throw errorObject(404, "user not found");
-      }
-      const userWithoutPassword = { ...user.toObject(), password: undefined };
-
-      return userWithoutPassword;
-    } catch (error) {
-      errorObject(401, "Invalid token");
-    }
+//get user by token
+export const getUserByToken = async (token: string) => {
+  if (!token) {
+    throw errorObject(401, "Access denied. No token provided");
   }
-
-  // create user
-  async createUser(userData: UserBody) {
-    // validate request
-    const { name, email, password } = userData;
-    if (!(email && password && name)) {
-      throw errorObject(400, "All input is required");
-    }
-
-    // check if user already exist
-    const oldUser = await UserModel.findOne({ email });
-    if (oldUser) {
-      throw errorObject(409, "User already exist. Please login");
-    }
-
-    // encrypt user password
-    const encryptedPassword = await bcrypt.hash(
-      password,
-      parseInt(config.HASH_STEPS)
-    );
-
-    // create user in db
-    const user = await UserModel.create({
-      name,
-      email: email.toLowerCase(),
-      password: encryptedPassword,
-      uuid: uuid(),
-    });
-
-    // create token
-    const token = jwt.sign({ user_id: user._id, email }, config.JWT_SECRET, {
-      expiresIn: "24h",
-    });
-    const expireDate = new Date().setDate(new Date().getDate() + 1);
-
-    //return new user
-    user.save();
-    return { token, expireDate };
-  }
-
-  //login user
-  async login(userData: UserBody) {
-    const { email, password } = userData;
-    if (!(email && password)) {
-      throw errorObject(400, "All input is required");
-    }
-    const user = await UserModel.findOne({ email })
-      .populate("restaurant")
-      .exec();
-
+  try {
+    const decoded = jwt.verify(token, config.JWT_SECRET) as Decoded;
+    const user = await UserModel.findById(decoded.user_id).exec();
     if (!user) {
-      throw errorObject(404, "Invalid credentials");
+      throw errorObject(404, "user not found");
     }
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) {
-      throw errorObject(404, "Invalid credentials");
-    }
-    const token = jwt.sign({ user_id: user._id, email }, config.JWT_SECRET, {
-      expiresIn: "24h",
-    });
-    const expireDate = new Date().setDate(new Date().getDate() + 1);
-    const userInfo = user.toObject();
-    return {
-      token,
-      expireDate,
-      userInfo: { ...userInfo, password: undefined },
-    };
+    const userWithoutPassword = { ...user.toObject(), password: undefined };
+
+    return userWithoutPassword;
+  } catch (error) {
+    errorObject(401, "Invalid token");
   }
-}
+};
+
+// get user by id
+export const getUserById = async (userId: string) => {
+  //isMongoIdValidation([userId]);
+  let user;
+  const isMongoId = mongoose.Types.ObjectId.isValid(userId);
+  if (!isMongoId) {
+    user = await UserModel.findOne({ email: userId }).exec();
+    if (!user) {
+      throw errorObject(400, "Invalid user id");
+    }
+    return user;
+  }
+  user = await UserModel.findById(userId).exec();
+  if (!user) {
+    throw errorObject(404, "User not found");
+  }
+  return user;
+};
+
+// create user
+export const createUser = async (userData: UserBody) => {
+  // validate request
+  const { name, email, password } = userData;
+  if (!(email && password && name)) {
+    throw errorObject(400, "All input is required");
+  }
+
+  // check if user already exist
+  const oldUser = await UserModel.findOne({ email });
+  if (oldUser) {
+    throw errorObject(409, "User already exist. Please login");
+  }
+
+  // encrypt user password
+  const encryptedPassword = await bcrypt.hash(
+    password,
+    parseInt(config.HASH_STEPS)
+  );
+
+  // create user in db
+  const user = await UserModel.create({
+    name,
+    email: email.toLowerCase(),
+    password: encryptedPassword,
+  });
+
+  // create token
+  const token = jwt.sign({ user_id: user._id, email }, config.JWT_SECRET, {
+    expiresIn: "24h",
+  });
+  const expireDate = new Date().setDate(new Date().getDate() + 1);
+
+  //return new user
+  user.save();
+  return { token, expireDate };
+};
+
+//login user
+export const login = async (userData: UserBody) => {
+  const { email, password } = userData;
+  if (!(email && password)) {
+    throw errorObject(400, "All input is required");
+  }
+  const user = await UserModel.findOne({ email }).populate("restaurant").exec();
+
+  if (!user) {
+    throw errorObject(404, "Invalid credentials");
+  }
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    throw errorObject(404, "Invalid credentials");
+  }
+  const token = jwt.sign({ user_id: user._id, email }, config.JWT_SECRET, {
+    expiresIn: "24h",
+  });
+  const expireDate = new Date().setDate(new Date().getDate() + 1);
+  const userInfo = user.toObject();
+  return {
+    token,
+    expireDate,
+    userInfo: { ...userInfo, password: undefined },
+  };
+};
