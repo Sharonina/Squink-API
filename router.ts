b1ce4b525af3c99140c58469d7458daff196b760
Sharonina <shar_mdza@hotmail.com>
2023-03-23 19:45:06 -0600

    descomentanto ruta de notes

--- router.ts
+++ router.ts
@@ -1,8 +1,8 @@
 import { Express } from "express";
 import UserRouter from "./modules/users/routes/user.route";
-/* import NotesRouter from "./modules/notes/routes/note.route"; */
+import NotesRouter from "./modules/notes/routes/note.route";
 
 export default function routerApi(app: Express) {
   app.use("/users", UserRouter);
-  /* app.use("/notes", NotesRouter); */
+  app.use("/notes", NotesRouter);
 }
